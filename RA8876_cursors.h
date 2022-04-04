9463ef8700ca7565b063ca350eaba83f8ec38d1a
gfcwfzkm <gfcwfzkm@protonmail.com>
2022-04-04 13:18:09 +0200

    Added example cursors and touch drivers

---
+++ RA8876_cursors.h
@@ -0,0 +1,17 @@
+/*
+ * RA8876_cursors.h
+ * Four cursors to load!
+ * Created: 01.12.2020 14:03:44
+ *  Author: gfcwfzkm
+ */ 
+
+
+#ifndef RA8876_CURSORS_H_
+#define RA8876_CURSORS_H_
+
+extern const unsigned char __memx RA8876_gImage_busy_im[256];
+extern const unsigned char __memx RA8876_gImage_no_im[256];
+extern const unsigned char __memx RA8876_gImage_arrow_il[256];
+extern const unsigned char __memx RA8876_gImage_pen_il[256];
+
+#endif /* GRAPH_CURSOR_H_ */
